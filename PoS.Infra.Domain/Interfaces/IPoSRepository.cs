ffc38e1b79a0f10791aa416f4e520736e36e5713
Linguist-Codifier <myselfcontact.jonathan@gmail.com>
2023-10-24 22:16:06 -0300

    adding common infra domain assembly

---
+++ PoS.Infra.Domain/Interfaces/IPoSRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace PoS.Infra.Domain.Interfaces
+{
+    /// <summary>
+    /// Determines low-level properties, and models that any <see cref="IPoSRepository"/> implementation must have.
+    /// </summary>
+    public interface IPoSRepository { }
+}
---
+++ PoS.Infra.Domain/Interfaces/IPoSServiceDbContext.cs
@@ -0,0 +1,7 @@
+﻿namespace PoS.Infra.Domain.Interfaces
+{
+    public interface IPoSServiceDbContext<PoSServiceRepository> where PoSServiceRepository : IPoSRepository
+    {
+        public PoSServiceRepository Repository { get; }
+    }
+}
