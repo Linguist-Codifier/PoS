a02588da422000a6c7d36e4fde7e2978988e7d4b
Linguist-Codifier <myselfcontact.jonathan@gmail.com>
2023-10-24 22:14:05 -0300

    adding common infra assembly

---
+++ PoS.Infra/Context/PoSServiceDbContext.cs
@@ -0,0 +1,13 @@
+﻿using PoS.Infra.Domain.Interfaces;
+
+namespace PoS.Infra.Context
+{
+    public class PoSServiceDbContext<PoSServiceRepository> : IPoSServiceDbContext<PoSServiceRepository> where PoSServiceRepository : IPoSRepository
+    {
+        private readonly PoSServiceRepository serviceScopeRepositoryInstance;
+
+        PoSServiceRepository IPoSServiceDbContext<PoSServiceRepository>.Repository { get => this.serviceScopeRepositoryInstance; }
+
+        public PoSServiceDbContext(PoSServiceRepository poSServiceRepositoryInstance) => this.serviceScopeRepositoryInstance = poSServiceRepositoryInstance;
+    }
+}
